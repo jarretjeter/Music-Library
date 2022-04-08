9c39912767f6eed12a44e2d4bac9a2550a2327eb
Jarret Jeter <jarretjeter@yahoo.com>
2022-04-08 10:42:36 -0700

    add local 'id_number' variable to 'Artist' class

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-class Artists:
-    def __init__(self, id, name, genre, birth_date):
-        self.id = id
+class Artist:  
+    id_number = 000
+
+    def __init__(self, name, genre, birth_date):
+        self.id = Artist.id_number
+        Artist.id_number = Artist.id_number + 1
         self.name = name
         self.genre = genre
         self.birth_date = birth_date
 
-class Songs:
+class Song:
     def __init__(self, artist, title, length, lyrics):
         self.artist = artist
         self.title = title
         self.length = length
         self.lyrics = lyrics
+
+mj = Artist("Michael Jackson", "Pop", "August 29, 1958")
+print(mj.genre)
+print(mj.id)
+
+tc = Artist("Thundercat", "R&B", "October 19, 1984")
+print(tc.birth_date)
+print(tc.id)
