4316e9c1164cf1d22ec57f56f3ff9dfa6c525e2f
Jarret Jeter <jarretjeter@yahoo.com>
2022-04-08 14:17:27 -0700

    WIP

--- main.py
+++ main.py
@@ -17,6 +17,13 @@ class Artist:
     def __repr__(self):
         return f"{self.artist}, {self.genre}, {self.birth_date}"
 
+    def has_artist(self):
+        self.artist = artist
+        if self.artist in Artist.artists:
+            return f"{self.artist} is in the list of artists"
+        else:
+            return f"{self.artist} is not currently in the list of artists"
+
 class Song:
     def __init__(self, title, length, lyrics):
         artist = Artist.id_number
@@ -42,3 +49,5 @@ print(artists)
 
 them_changes = Song("Them Changes", "3:08", "Nobody move, there's blood on the floor, And I cant find my heart, Where did it go? Did I leave it in the cold? So please give it back, 'cause it's not yours to take. It must've fell when I lost my mind. Deep in the cut, drowning in a pain. Somebody help, 'cause I can't find my way Nobody move, nobody move. Somebody tell me how I'm supposed to feel. When I'm sitting here knowing this ain't real. Why in the world would I give my heart to you? Just to watch you throw it in the trash. I've been traveling so long, I don't think I can hold on. Where were you when I needed you the most? Now I'm sitting here with a black hole in my chest. A heartless, broken mess")
 print(them_changes)
+
+Artist.has_artist("Michael Jackson")
