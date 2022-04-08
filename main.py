3ff1343a5204b7ffec901814283fa8157d33740a
Jarret Jeter <jarretjeter@yahoo.com>
2022-04-08 09:35:23 -0700

    add 'Artists' class

---
+++ main.py
@@ -0,0 +1,6 @@
+class Artists:
+    def __init__(self, id, name, genre, birth_date):
+        self.id = id
+        self.name = name
+        self.genre = genre
+        self.birth_date = birth_date
