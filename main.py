2c2469248db45f2604a13bf91bd4a595f69b383b
Jarret Jeter <jarretjeter@yahoo.com>
2022-04-08 12:01:35 -0700

    add ValueError when creating artist class instance if artist already exists

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 class Artist:  
-    id_number = 100
 
-    def __init__(self, name, genre, birth_date):
+    id_number = 100
+    artists = []
+
+    def __init__(self, artist, genre, birth_date):
         self.id = Artist.id_number
         Artist.id_number = Artist.id_number + 1
-        self.name = name
+        if artist in Artist.artists:
+            raise ValueError(f"Artist '{artist}' already exists.")
+        else:
+            self.artist = artist
+            Artist.artists.append(artist)
         self.genre = genre
         self.birth_date = birth_date
 
     def __repr__(self):
-        return f"{self.name}, {self.genre}, {self.birth_date}"
+        return f"{self.artist}, {self.genre}, {self.birth_date}"
 
 class Song:
     def __init__(self, artist, title, length, lyrics):
@@ -25,6 +31,8 @@ mj = Artist("Michael Jackson", "Pop", "August 29, 1958")
 print(mj.genre)
 print(mj.id)
 print(mj)
+
+mjackson = Artist("Michael Jackson", "Pop", "August 29, 1958")
 
 tc = Artist("Thundercat", "R&B", "October 19, 1984")
 print(tc.birth_date)
